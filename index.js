46a2ec6c4fc5ee1bfda75c7cf55880d913e4d63f
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-03-15 23:37:39 +0700

    Simple http module Node.js server

---
+++ index.js
@@ -0,0 +1,18 @@
+// The http module for Node.js server
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, err => {
+  if (err) {
+    return console.log('Something bad happened', err)
+  }
+
+  console.log(`Server is listening on ${port}`)
+})
