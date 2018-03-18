5b1ebe715896385630296474dd1bc20091af634b
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-03-18 23:38:16 +0700

    Using database postgresql

--- index.js
+++ index.js
@@ -1,69 +1,46 @@
 const express = require('express')
-const pg = require('pg')
+const { Client } = require('pg')
+const connectionString = 'postgres://dangvanthanh@localhost/learning_objectives'
+const client = new Client({
+  connectionString: connectionString
+})
 const app = express()
 const port = 3000
-
-const conString = 'postgres://username:password@localhost/learning_objectives'
-
-// pg.connect(conString, (err, client, done) => {
-//   if (err) {
-//     return console.error('Error fetching client from pool', err)
-//   }
-
-//   client.query(
-//     'SELECT $1::varchar AS my_first_query',
-//     ['learning_objectives'],
-//     (err, result) => {
-//       done()
-
-//       if (err) {
-//         return console.error('Error happened during query', err)
-//       }
-
-//       console.log(result.rows[0])
-//       process.exit()
-//     }
-//   )
-// })
 
 app.post('/users', (req, res, next) => {
   const user = req.body
 
-  pg.connect(conString, (err, client, done) => {
-    if (err) {
-      return next(err)
-    }
+  client.connect()
 
-    client.query(
-      'INSERT INTO users (name, age) VALUES ($1, $2);',
-      [user.name, user.age],
-      (err, result) => {
-        done()
-
-        if (err) {
-          return next(err)
-        }
-
-        res.send(200)
+  client.query(
+    'INSERT INTO users (name, age) VALUES ($1, $2);',
+    [user.name, user.age],
+    (err, result) => {
+      if (err) {
+        console.log(err)
       }
-    )
-  })
+
+      res.send(200)
+    }
+  )
 })
 
 app.get('/users', (req, res, next) => {
-  pg.connect(conString, (err, client, done) => {
+  client.connect()
+
+  client.query('SELECT name, age FROM users;', [], (err, result) => {
     if (err) {
-      return next(err)
+      console.log(err)
     }
 
-    client.query('SELECT name, age FROM users;', [], (err, result) => {
-      done()
-
-      if (err) {
-        return next(err)
-      }
-
-      res.json(result.rows)
-    })
+    res.json(result.rows)
   })
+})
+
+app.listen(port, err => {
+  if (err) {
+    return console.log('Something bad happened', err)
+  }
+
+  console.log(`Server is listening on ${port}`)
 })
